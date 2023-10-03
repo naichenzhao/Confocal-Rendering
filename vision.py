e2d71bff3f0d7b70c6e4cc665713db4d504f7bc9
Naichen Zhao <nzhao11235@berkeley.edu>
2023-10-03 13:14:00 -0700

    updated vision.py

--- vision.py
+++ vision.py
@@ -5,6 +5,9 @@ import os
 from tqdm import tqdm
 import mcubes
 import cv2 as cv
+from aspose.threed.utilities import Vector3
+from aspose.threed.entities import PolygonModifier, Box
+from aspose.threed import Scene
 
 from skimage import *
 import skimage.io as io
@@ -23,7 +26,7 @@ def main():
 
 
     length = len(tif_files)
-    mat = np.zeros((512, 512, length))
+    mat = np.zeros((512, 512, length+20))
 
     count = 0
     for file in tqdm(tif_files):
@@ -31,7 +34,7 @@ def main():
         curr_file = os.path.join(img_dir, file_name)
         curr_img = cv.cvtColor(cv.imread(curr_file), cv.COLOR_BGR2GRAY)
 
-        mat[:, :, count] = process_img("zlevel_{0}".format(count), curr_img)
+        mat[:, :, count+10] = process_img("zlevel_{0}".format(count), curr_img)
         count += 1
     print('Images Processed')
 
@@ -42,11 +45,19 @@ def main():
     vertices, triangles = mcubes.marching_cubes(mat, 0)
     mcubes.export_obj(vertices, triangles, "output.obj")
 
+    # print('Converting Model to stl')
+    # scene = Scene.from_file("output.obj")
+    # scene.save("output.stl")
+
 
     print('Generating Smoothed 3D model')
     smoothed_mat = mcubes.smooth(mat, sigma = 1)
     vertices, triangles = mcubes.marching_cubes(smoothed_mat, 0)
     mcubes.export_obj(vertices, triangles, "output_sm.obj")
+
+    # print('Converting Smoothed Model to stl')
+    # scene = Scene.from_file("tmp/output_sm.obj")
+    # scene.save("output_sm.stl")
 
 
     print('Program Finished')
